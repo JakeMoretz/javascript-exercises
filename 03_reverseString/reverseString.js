3202a03f1f46e263d77dd0f31c960b5c7b50303a
Jake Moretz <jmoretz9090@gmail.com>
2023-01-01 22:27:19 -0500

    testing for exercises 1-3 complete with comment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {  // add argument str
+    
+    //empty string loop
+    let  newString = "";   // empty string var is created
+    for(let i = str.length - 1; i >= 0; i--) {         //str.length - 1 gives the position of the last element
+        newString += str[i];                           // that element is added to the new string var newString
+    }                                                  // the value of i decreases by 1 each time the loop runs until it reaches 0
+    return newString;
 };
 
 // Do not edit below this line
