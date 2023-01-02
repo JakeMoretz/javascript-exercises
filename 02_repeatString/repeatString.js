c78604f60a1fe1f8baccea20d002f750c0770cc6
Jake Moretz <jmoretz9090@gmail.com>
2023-01-01 22:02:36 -0500

    completed the first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let accumulatedstr = ""; // create empty string variable
+    for (let i = 0; i < num; i++) {  // use for loop to run as many times at the num
+        accumulatedstr += str;  // add string to new variable
+    }
+    return accumulatedstr;  // return variable for repeated string 
 };
 
 // Do not edit below this line
