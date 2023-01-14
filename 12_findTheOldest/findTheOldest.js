71b62068da6d7ab140b907e05de630b6d59206b9
Jake Moretz <jmoretz9090@gmail.com>
2023-01-14 12:43:08 -0500

    exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+    const oldest = people.sort((a, b) =>
+      age(a) > age(b) ? -1 : 1
+    );
+    return oldest[0];
 };
 
 // Do not edit below this line
