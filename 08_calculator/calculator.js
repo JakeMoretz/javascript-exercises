13bbd32ed846191d87e2304f2812c3ae36a56d63
Jake Moretz <jmoretz9090@gmail.com>
2023-01-14 11:42:01 -0500

    complete calculator exercises' '

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      sum += array[i]
+    }
+    return(sum);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    let sum = 1;
+    for (let i = 0; i<array.length; i++) {
+      sum = sum * array[i];
+    }
+    return sum;
 
 };
 
-const power = function() {
-	
+const power = function(m, n) {
+	return Math.pow(m, n);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return(n<2)?1:factorial(n-1)*n;
 };
 
 // Do not edit below this line
